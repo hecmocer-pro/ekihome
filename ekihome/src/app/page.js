649c324b7e389626b6c15dcced00943f577701de
Hector Moreno <hecmocer.pro@gmail.com>
2025-01-23 20:33:10 +0100

    working on the structure yet

--- ekihome/src/app/page.js
+++ ekihome/src/app/page.js
@@ -1,101 +1,82 @@
-import Image from "next/image";
+import React from 'react';
+import './styles.css';
 
-export default function Home() {
+function App() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div>
+      <header>
+        <h1>Ekihome</h1>
+        <nav>
+          <ul>
+            <li><a href="#Page1">Page1</a></li>
+            <li><a href="#Page2">Page2</a></li>
+            <li><a href="#Page3">Page3</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section id="video">
+          <div className="landing-video-container">
+            <video loop autoPlay muted className="landing-video">
+              <source src="/landing.mp4" type="video/mp4" />
+              Tu navegador no soporta la etiqueta de video.
+            </video>
+          </div>
+        </section>
+        <section>
+          <div className="marketing-container">
+            <div className="text-container">
+              <h3>lorem</h3>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+            </div>
+            <div className="text-container">
+              <h3>lorem</h3>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+            </div>
+            <div className="text-container">
+              <h3>lorem</h3>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="pictures-container">
+            <div className="image1"></div>
+            <div className="image2"></div>
+            <div className="image3"></div>
+          </div>
+        </section>
+        <section>
+          Waitlist
+        </section>
+        {/* <section id="intro">
+          <h2>Explore the latest trends</h2>
+          <p>Join us for two thrilling days of talks, keynotes, and panels!</p>
+        </section>
+        <section id="tickets">
+          <h2>Get your tickets</h2>
+          <p>Buy tickets now and assign them to attendees later.</p>
+          <ul>
+            <li>Early Ticket: 89€ + VAT</li>
+            <li>Final Price: 189€ + VAT</li>
+            <li>Group Ticket: 76€ + VAT (3 or more tickets)</li>
+            <li>Student Ticket: 69€ + VAT (Student ID required)</li>
+          </ul>
+        </section>
+        <section id="sponsor">
+          <h2>Become a Sponsor</h2>
+          <p>Connect with talents from all over the world and get to know speakers, tech leaders, and innovative companies.</p>
+        </section>
+        <section id="speakers">
+          <h2>Call for Speakers</h2>
+          <p>Share your knowledge and shape the future with us.</p>
+        </section> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+        <p>&copy; Ekihome. All rights reserved.</p>
       </footer>
     </div>
   );
 }
+
+export default App;
