31d8165def6cceb75aa072942d1f5e514b43154c
Hector Moreno <hecmocer.pro@gmail.com>
2025-01-25 13:22:47 +0100

    feat: estructura general, solo faltaria la parte de la waitlist y algun que otro enlace

--- ekihome/src/app/page.js
+++ ekihome/src/app/page.js
@@ -1,80 +1,100 @@
 import React from 'react';
 import './styles.css';
+import Button from '@mui/material/Button';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
     <div>
-      <header>
-        <h1>Ekihome</h1>
-        <nav>
-          <ul>
-            <li><a href="#Page1">Page1</a></li>
-            <li><a href="#Page2">Page2</a></li>
-            <li><a href="#Page3">Page3</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <section id="video">
-          <div className="landing-video-container">
-            <video loop autoPlay muted className="landing-video">
-              <source src="/landing.mp4" type="video/mp4" />
-              Tu navegador no soporta la etiqueta de video.
-            </video>
-          </div>
-        </section>
-        <section>
-          <div className="marketing-container">
-            <div className="text-container">
-              <h3>lorem</h3>
-              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+
+      <div className="pageframe">
+
+        <header className="header">
+          <nav className="navbar">
+            <img src="/ekihome-nobg.png" alt="ekihome-logo" className="ekihome-logo" />
+            <div>
+              <h1 className="ekihome-text">Ekihome</h1>
+              <h2 className="ekihome-subtext">Residencia ecuestre</h2>
             </div>
-            <div className="text-container">
-              <h3>lorem</h3>
-              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+            {/* <ul className="links">
+              <li className="link"><a href="#waitlist">Lista de espera</a></li>
+              <li className="link"><a href="#footer">Contacto</a></li>
+              <li className="link"><a href="#footer">Acceso</a></li>
+            </ul> */}
+          </nav>
+        </header>
+        <main>
+          <section id="video">
+            <div className="landing-video-container">
+              <video loop autoPlay muted className="landing-video">
+                <source src="/landing.mp4" type="video/mp4" />
+                Tu navegador no soporta la etiqueta de video.
+              </video>
             </div>
-            <div className="text-container">
-              <h3>lorem</h3>
-              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni accusamus nemo sapiente aliquam. Deserunt libero sunt sit numquam maxime accusamus dolore error odio vero perferendis! Harum repellendus deleniti atque rem.</div>
+          </section>
+          <section id="intro">
+            <div className="intro-container">
+              <div className="text-container">
+                <h3>Instalaciones de primera clase</h3>
+                <div>Nuestras instalaciones están diseñadas para proporcionar el máximo confort y seguridad a tu caballo, con amplios establos y prados verdes.</div>
+              </div>
+              <div className="text-container">
+                <h3>Cuidado personalizado</h3>
+                <div>Ofrecemos una gama completa de servicios, desde alimentación y limpieza hasta atención veterinaria y programas de rehabilitación.</div>
+              </div>
+              <div className="text-container">
+                <h3>Ambiente tranquilo</h3>
+                <div>Rodeado de naturaleza, nuestro centro ecuestre es el lugar ideal para que tu caballo se relaje y recupere en un entorno sereno.</div>
+              </div>
             </div>
-          </div>
-        </section>
-        <section>
-          <div className="pictures-container">
-            <div className="image1"></div>
-            <div className="image2"></div>
-            <div className="image3"></div>
-          </div>
-        </section>
-        <section>
-          Waitlist
-        </section>
-        {/* <section id="intro">
-          <h2>Explore the latest trends</h2>
-          <p>Join us for two thrilling days of talks, keynotes, and panels!</p>
-        </section>
-        <section id="tickets">
-          <h2>Get your tickets</h2>
-          <p>Buy tickets now and assign them to attendees later.</p>
-          <ul>
-            <li>Early Ticket: 89€ + VAT</li>
-            <li>Final Price: 189€ + VAT</li>
-            <li>Group Ticket: 76€ + VAT (3 or more tickets)</li>
-            <li>Student Ticket: 69€ + VAT (Student ID required)</li>
-          </ul>
-        </section>
-        <section id="sponsor">
-          <h2>Become a Sponsor</h2>
-          <p>Connect with talents from all over the world and get to know speakers, tech leaders, and innovative companies.</p>
-        </section>
-        <section id="speakers">
-          <h2>Call for Speakers</h2>
-          <p>Share your knowledge and shape the future with us.</p>
-        </section> */}
-      </main>
-      <footer>
-        <p>&copy; Ekihome. All rights reserved.</p>
-      </footer>
+          </section>
+          <section id="pictures">
+            <div className="pictures-container">
+              <div className="image1"></div>
+              <div className="image2"></div>
+              <div className="image3"></div>
+            </div>
+          </section>
+          <section id="opinions">
+            <div className="opinions-container">
+              <h3>Esto opinan nuestros clientes</h3>
+              <div>
+                <p className="opinion">"Ekihome Residencia Ecuestre es el lugar perfecto para mi caballo. Las instalaciones son de primera clase, el ambiente es tranquilo y el cuidado personalizado es excepcional. ¡No podría estar más satisfecho!" - Laura Fernández</p>
+                <p className="opinion">"Desde que descubrí Ekihome Residencia Ecuestre, mi caballo ha encontrado su segundo hogar. El ambiente tranquilo y las instalaciones de primera clase son inigualables. ¡Recomiendo este lugar a todos los amantes de los caballos!" - Carlos Martínez</p>
+                <p className="opinion">"Ekihome Residencia Ecuestre ha superado todas mis expectativas. El cuidado y la atención que brindan a los caballos es inigualable. ¡Mi caballo nunca ha estado más feliz y relajado!" - Ana Gómez</p>
+              </div>
+            </div>
+          </section>
+          <section id="video2">
+            <div className="secondary-video-container">
+              <video loop autoPlay muted className="secondary-video">
+                <source src="/Ekihomev8.mp4" type="video/mp4" />
+                Tu navegador no soporta la etiqueta de video.
+              </video>
+            </div>
+          </section>
+          <section id="waitlist">
+            <h1>Únete a la lista de espera</h1>
+            {/* <Button variant="contained">Waitlist</Button> */}
+          </section>
+          <section id="summary">
+            <p>El hogar para <u>tu caballo y tú</u></p>
+          </section>
+        </main>
+        <footer id="footer">
+          <div className="copyright"><p>&copy; Copyright Ekihome S.p.A. 28690 Brunete, Madrid | <a href="https://maps.app.goo.gl/JVDP1Uz6HXPURVXMA">Ubicación</a> | <a href="mailto:000">Contacto</a></p></div>
+          <div><a href="https://www.instagram.com/ekihome_residenciaecuestre"><img src="/instagram.png" alt="Instagram" className="logo" /></a></div>
+        </footer>
+      </div>
     </div>
   );
 }
